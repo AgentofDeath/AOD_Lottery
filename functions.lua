4c62573f92b7f83ba48046fa71431f96a9efc404
AgentofDeath <69638603+AgentofDeath@users.noreply.github.com>
2021-06-22 15:30:06 -0500

    init progress

--- functions.lua
+++ functions.lua
@@ -1,3 +1,25 @@
 --to do.
 
 -- Make script not rely on esx 1.1.
+
+
+HasMoney = function(source)
+  local xPlayer = ESX.GetPlayerFromId(source)
+    if xPlayer.get("money") >= 500 --use me for esx 1.1
+    --if xPlayer.getAccount("money") >= 500 then --use this option for versions not esx 1.1
+      return true
+    else
+      return false
+    end
+
+RemoveCash = function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    xPlayer.removeMoney(500)
+    --xPlayer.removeAccountMoney("money", 500) --for versions above esx 1.1
+end
+  
+AddCash = function(source, amount)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    xPlayer.addMoney(amount)
+    --xPlayer.addAccountMoney("bank", amount)
+end
